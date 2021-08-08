0c157060ef74c837bb9c681fd8987205bfbef94f
Dora-emond <85222479+Dora-emond@users.noreply.github.com>
2021-08-08 10:28:00 +0700

    Update pay.js

--- plugins/pay.js
+++ plugins/pay.js
@@ -1,12 +1,12 @@
 let { MessageType } = require('@adiwajshing/baileys')
 let pajak = 0.02
 let handler = async (m, { conn, text }) => {
-  if (!text) throw 'Masukkan jumlah exp yang akan diberi'
   let who
   if (m.isGroup) who = m.mentionedJid[0]
   else who = m.chat
   if (!who) throw 'Tag salah satu lah'
   let txt = text.replace('@' + who.split`@`[0], '').trim()
+  if (!txt) throw 'Masukkan jumlah exp yang akan diberi'
   if (isNaN(txt)) throw 'Hanya angka'
   let xp = parseInt(txt)
   let exp = xp
@@ -27,4 +27,3 @@ handler.command = /^pay$/
 handler.rowner = true
 
 module.exports = handler
-
