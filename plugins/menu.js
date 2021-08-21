f72e7cb646ad9d76dd0ebb2aa2c430f56e46a56b
Dora-emond <85222479+Dora-emond@users.noreply.github.com>
2021-08-21 14:17:03 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let path = require('path')
 let levelling = require('../lib/levelling')
-let handler = require('../src/Logobot.jpg')
+let handler = async('../src/Logobot.jpg')
 let tags = {
   'main': 'Main',
   'game': 'Game',
