310a94d5c46d25caf6d2943d38bd8bc78a3b7b42
Dora-emond <85222479+Dora-emond@users.noreply.github.com>
2021-08-22 11:36:38 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -156,7 +156,8 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
     conn.reply(m.chat, text.trim(), m)
-  } catch (e) {
+    await conn.send2Button(m.chat, text.trim(), 'made with ❤️ by ariffb', 'PEMILIK BOT', '.owner', 'DONASI', '.donasi', { quoted: m })
+    } catch (e) {
     conn.reply(m.chat, 'Maaf, menu sedang error', m)
     throw e
   }
