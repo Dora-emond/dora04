2c7f413ea117198cfd9153d6494b2a2951cb9416
Dora-emond <85222479+Dora-emond@users.noreply.github.com>
2021-08-25 13:44:16 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
-  // F this.sendContact(m.chat, '6282387704390', 'Nurutomo', m)
+  this.sendContact(m.chat, '6282387704390', 'Nurutomo', m)
   this.sendContact(m.chat, '6289601145546', 'sahabatku Dora :D', m)
 }
 handler.help = ['owner', 'creator']
