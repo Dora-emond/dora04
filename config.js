6086cf606689a3735b615079011e0705d4a79a6c
Dora-emond <85222479+Dora-emond@users.noreply.github.com>
2021-10-30 07:59:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ global.APIs = { // API Prefix
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
-  'https://api.xteam.xyz': 'HIRO',
+  'https://api.xteam.xyz': 'seventszid123',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
   'https://api.zeks.xyz': 'apivinz',
   'https://pencarikode.xyz': 'ppais'
